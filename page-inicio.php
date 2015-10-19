b08ae80e1d85cbb4ffa8753dedfe6120d0f6e5f8
Ludo Bermejo <ludo.bermejo@starzplayarabia.com>
2015-10-19 10:36:21 +0200

    First steps to article by category

--- page-inicio.php
+++ page-inicio.php
@@ -30,15 +30,47 @@
                     </div>
 
                     <ul class="sumary">
-
-
+                        <!--<li class="title"><a href="<?php echo get_permalink(12) ?>">Prólogo</a></li>-->
                         <?php
 
-                        $line = '<li><a class="issuem_article_link" href="%URL%"><strong>%TITLE%</strong>%BYLINE%</a></li>';
-                        echo get_issuem_articles_free_form("", $line);
-                        ?>
+                        $special = array(
+                            'article_category'		=> 'articulos'
+                        );
 
+
+                        $line = '<li><a class="issuem_article_link" href="%URL%"><strong>%TITLE%</strong>%BYLINE%</a></li>';
+                        echo get_issuem_articles_free_form($special, $line);
+                        ?>
                     </ul>
+                    <strong>Entrevistas</strong>
+                    <ul class="sumary">
+                        <!--<li class="title"><a href="<?php echo get_permalink(12) ?>">Prólogo</a></li>-->
+                        <?php
+
+                        $special = array(
+                            'article_category'		=> 'entrevistas'
+                        );
+
+
+                        $line = '<li><a class="issuem_article_link" href="%URL%"><strong>%TITLE%</strong>%BYLINE%</a></li>';
+                        echo get_issuem_articles_free_form($special, $line);
+                        ?>
+                    </ul>
+                    <strong>Cuentos</strong>
+                    <ul class="sumary">
+                        <!--<li class="title"><a href="<?php echo get_permalink(12) ?>">Prólogo</a></li>-->
+                        <?php
+
+                        $special = array(
+                            'article_category'		=> 'cuentos'
+                        );
+
+
+                        $line = '<li><a class="issuem_article_link" href="%URL%"><strong>%TITLE%</strong>%BYLINE%</a></li>';
+                        echo get_issuem_articles_free_form($special, $line);
+                        ?>
+                    </ul>
+
 
                     <div class="carrousel">
                         <div class="issues">
