a260db04e4230fe1e11e40199b077216162f6fb7
Ludo Bermejo <ludo.bermejo@starzplayarabia.com>
2016-01-18 14:18:09 +0100

    Add logo to img

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <title>A sangre y letras, revista literaria</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class()?>>
     <div class="wrapper">
