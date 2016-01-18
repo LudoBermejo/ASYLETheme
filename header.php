54bb8973e8ec74eaebc2b7399f675db59d0c7dff
Daniel Arenas <daniel.arenas@tecnilogica.com>
2016-01-18 23:23:06 +0100

    burger menu

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:image" content="http://www.asangreyletras.es/wp-content/uploads/2016/01/asyle_logo3.jpg" />
     <?php wp_head(); ?>
-    <script src="http://localhost/asyle/wp-content/themes/asyle/lib/jquery/jquery-1.11.2.min.js"></script>
-    <script src="http://localhost/asyle/wp-content/themes/asyle/js/interaction.js"></script>
+    <script src="/asyle/wp-content/themes/asyle/lib/jquery/jquery-1.11.2.min.js"></script>
+    <script src="/asyle/wp-content/themes/asyle/js/interaction.js"></script>
 
 </head>
 <body <?php body_class()?>>
@@ -18,7 +18,7 @@
                 <div class="logo">
                     <a href="<?php echo get_site_url();?> ">as<span class="icon-and"></span>le</a>
                 </div>
-                <button class="menu-trigger"><img src="http://localhost/asyle/wp-content/themes/asyle/img/burger-icon.png"></button>
+                <button class="menu-trigger"><img src="/asyle/wp-content/themes/asyle/img/burger-icon.png"></button>
 
                 <?php if (function_exists(clean_custom_menus)) clean_custom_menus("primary", get_permalink( $post->ID )); ?>
 
