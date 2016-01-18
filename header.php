89070624a530620927394f747d039174253f2fa3
Ludo Bermejo <ludo.bermejo@starzplayarabia.com>
2016-01-18 13:28:45 +0100

    Change title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 <html>
 <head>
-    <title>As&AMP;le</title>
+    <title>A sangre y letras, revista literaria</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
