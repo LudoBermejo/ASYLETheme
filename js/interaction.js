3999da5b71b65f8e6dd2587b701709c8f32f5505
Daniel Arenas <daniel.arenas@tecnilogica.com>
2016-01-18 22:52:53 +0100

    burger menu

--- js/interaction.js
+++ js/interaction.js
@@ -1,8 +1,22 @@
-
+$( window ).resize(function() {
+  $(function () {
+    viewport = $(window).width();
+    if (viewport > 768 && !$('.nav').hasClass('active')) {
+      $('.nav').slideDown('active');
+    }
+  });
+});
 
 $(document).ready(function () {
   $('.menu-trigger').click(function() {
-    $('.nav').slideToggle('active');
-    console.log('entra');
+    if ($('.nav').hasClass('active') === true) {
+      $('.nav').slideUp();
+      $('.nav').removeClass('active');
+    }
+    else {
+      $('.nav').slideDown();
+      $('.nav').addClass('active');
+    }
+
   })
 });
