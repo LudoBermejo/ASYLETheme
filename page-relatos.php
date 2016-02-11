589fbb4cc1291d79a568c1961154c1825b825883
Ludo Bermejo <ludo.bermejo@starzplayarabia.com>
2016-02-11 11:42:43 +0100

    Adding resenas

--- page-relatos.php
+++ page-relatos.php
@@ -19,6 +19,22 @@
                 </ul>
                 </div>
             <div class="inicio-index">
+                <h2>Las reseñas</h2>
+                <ul class="names">
+                    <!--<li class="title"><a href="<?php echo get_permalink(12) ?>">Prólogo</a></li>-->
+                    <?php
+
+                    $special = array(
+                        'article_category'		=> 'resenas'
+                    );
+
+
+                    $line = '<li><a class="issuem_article_link" href="%URL%"><strong>%TITLE%</strong>, %BYLINE%</a></li>';
+                    echo get_issuem_articles_free_form($special, $line, "empty");
+                    ?>
+                </ul>
+            </div>
+            <div class="inicio-index">
                 <h2>Las entrevistas</h2>
                 <ul class="names">
                     <!--<li class="title"><a href="<?php echo get_permalink(12) ?>">Prólogo</a></li>-->
