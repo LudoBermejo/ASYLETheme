b29d8b3c3776428aa8fe0ef694b66d353255ccfa
Ludo Bermejo <ludo.bermejo@starzplayarabia.com>
2016-02-24 21:42:57 +0100

    Change in inicio page

--- page-inicio.php
+++ page-inicio.php
@@ -92,7 +92,8 @@
         </div>
         <div class="sidebar">
             <p><span class="icon-inicio wrapper-icon"></span></p>
-            <p><strong>A sangre y letras</strong> es una revista literaria creada por alumnos del <a href="http://escrituracreativa.com/">taller de escritura creativa de Clara Obligado</a></p>
+            <p><strong>A sangre y letras</strong> es una revista literaria creada por alumnos del <strong><a href="http://escrituracreativa.com/">taller de escritura creativa de Clara Obligado</a></strong></p>
+            <p>Si quieres saber más de nosotros <strong><a href="/la-revista/">pincha aquí</a></strong></p>
         </div>
     </div>
 <!--<script>
